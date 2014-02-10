8820d7673aba96fe0917558dc2d088c16a321968
Zoe <jinsuyi@gmail.com>
2014-02-10 20:02:16 +0100

    Exception handler

---
+++ MidiNumberException.java
@@ -0,0 +1,9 @@
+
+public class MidiNumberException extends Exception {
+	
+	
+	public MidiNumberException () {
+		super("Error! The stack must have at least 3 numbers for midi to play...");
+	    
+	}
+}
